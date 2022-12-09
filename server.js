9f9958a8f97394bf6d4ee6794f10a37433da6ddf
LamBuiF <45018997+LamBuiF@users.noreply.github.com>
2022-12-08 19:42:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/sw409-lam-bui'));
+app.use(express.static(__dirname + '/dist/routing-demo'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/sw409-lam-bui/index.html'));
+res.sendFile(path.join(__dirname+'/dist/routing-demo/index.html'));
 });
 app.listen(process.env.PORT || 8080);
