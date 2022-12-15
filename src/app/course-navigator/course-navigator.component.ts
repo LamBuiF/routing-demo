ea900f12db8703a6fe7153cf56c0ad69e954e1a5
LamBuiF <lam.bui@student.fairfield.edu>
2022-12-15 15:31:52 -0500

    Final project
    add ways to insert modules & lessons

--- src/app/course-navigator/course-navigator.component.ts
+++ src/app/course-navigator/course-navigator.component.ts
@@ -12,10 +12,20 @@ export class CourseNavigatorComponent implements OnInit {
   selectedModule = {lesson: [] as any[], id:""}
   courses: any[] | undefined
   CourseTitle: any
+  ModuleTitle: any
+  LessonTitle: any
   Author: any
 
   addCourse(CourseTitle: any, Author: any) {
     const course = this.courseServices.createCourse(CourseTitle, Author).subscribe((response: any) => {console.log(response)})
+  }
+
+  addModule(ModuleTitle: any) {
+    const module = this.courseServices.createModule(ModuleTitle, this.selectedCourse.id).subscribe((response: any) => {console.log(response)})
+  }
+
+  addLesson(LessonTitle: any) {
+    const lesson = this.courseServices.createLesson(LessonTitle, this.selectedModule.id).subscribe((response: any) => {console.log(response)})
   }
 
   constructor(private courseServices: CourseServices) {
--- src/services/CourseServices.ts
+++ src/services/CourseServices.ts
@@ -13,6 +13,7 @@ export class CourseServices{
 
   findAllCourses = () =>
     fetch("http://courseservice-env.eba-kc5r3dri.us-east-2.elasticbeanstalk.com/api/v1/courses/").then(Response => Response.json())
+    // fetch("http://localhost:8080/api/v1/courses/").then(Response => Response.json())
 
   createCourse(cTitle: string, cAuthor: string): Observable<any>{
 
@@ -20,7 +21,26 @@ export class CourseServices{
     console.log(u)
     const jobj = JSON.parse(u)
     return this.http.post<any>("http://courseservice-env.eba-kc5r3dri.us-east-2.elasticbeanstalk.com/api/v1/courses/", jobj)
+    // return this.http.post<any>("http://localhost:8080/api/v1/courses/", jobj)
 
+  }
+
+  createModule(mTitle: string, id: any): Observable<any>{
+
+    const u = '{"title": "' + mTitle + '"}'
+    console.log(u)
+    const jobj = JSON.parse(u)
+    return this.http.post<any>("http://courseservice-env.eba-kc5r3dri.us-east-2.elasticbeanstalk.com/api/v1/courses/", jobj)
+    // return this.http.patch<any>("http://localhost:8080/api/v1/courses/" + id, jobj)
+
+  }
+
+  createLesson(lTitle: string, id: any): Observable<any>{
+    const u = '{"title": "' + lTitle + '"}'
+    console.log(u)
+    const jobj = JSON.parse(u)
+    return this.http.post<any>("http://courseservice-env.eba-kc5r3dri.us-east-2.elasticbeanstalk.com/api/v1/modules/", jobj)
+    // return this.http.patch<any>("http://localhost:8080/api/v1/modules/" + id, jobj)
   }
 
 }
